c3fb805f58ffb2efe6e5c9cab6a5539840b513d7
Samantha Covington <samanthanc_08@yahoo.com>
2019-11-15 11:17:33 -0600

    Updates

---
+++ precache-manifest.863dfefd815c7d0dde7538e6d0a47c68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a56b09b47dbf819e436025f4ee8e29a",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "22e5f2ffc64eb33922af",
+    "url": "/Clicky-Game/static/css/main.4b33944b.chunk.css"
+  },
+  {
+    "revision": "80eaa54f4d19792c44c9",
+    "url": "/Clicky-Game/static/js/2.c148dbf4.chunk.js"
+  },
+  {
+    "revision": "22e5f2ffc64eb33922af",
+    "url": "/Clicky-Game/static/js/main.3cf59d6a.chunk.js"
+  },
+  {
+    "revision": "ac77e0aff6e05250fd73",
+    "url": "/Clicky-Game/static/js/runtime-main.0c69ceee.js"
+  }
+]);
